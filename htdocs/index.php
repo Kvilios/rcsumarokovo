788c90da80a6555fbf2081d190c793713eee23ac
Mikhail <kvilios@yandex.ru>
2018-08-01 11:50:21 +0300

    index bugs fix

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,38 +1,38 @@
 <!DOCTYPE html>
 <html lang="ru">
-  <body>
-    <div id="app"></div>
-  </body>
-  <script src="/js/main.js" charset="utf-8"></script>
-  <head>
-      <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-      <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-      <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-      <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-      <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-      <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-      <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-      <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-      <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-      <link rel="manifest" href="/manifest.json">
-      <meta name="msapplication-TileColor" content="#ffffff">
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-      <meta name="theme-color" content="#ffffff">
-      <meta charset="utf-8">
-      <meta name="format-detection" content="telephone=no">
-      <meta name="title" content="Помощь зависимым">
-      <meta name="keywords" content="костромская_митрополия, Скоропослушница, Литургия , молебен, катехизация, терапия, вера, наркозависимость, ресоциализация, адаптация, православная_реабилитация, скайп_группа, православие, кострома, итоги_недели, костромская_область, выздоровление, сумароково, взаимопомощь, реабилитация_наркозависимых, наркомания, алкоголизм, лечение_наркозависимых, группа_самопомощи, церковная помощь, группы_поддержки, мотивация, консультация, зависимость, помощь_зависимым, помощь_родственникам, наркореабилитация, алкореабилитация, здоровый_образ_жизни, адаптационная_квартира, реабилитационный_центр, реабилитация в другом регионе, трезвость, нет_наркотикам, без_наркотиков, верни_себе_жизнь, реабилитация_в_костроме, наркореабилитация_в_костроме">
-      <meta name="description" content="Помощь зависимым и их близким, церковная алко- и наркореабилитация">
-      <meta name="yandex-verification" content="5f434f86dd3e3a48" />
-      <meta name='wmail-verification' content='d7733a16c22ef0733ec321c297d3eb0d' />
-      <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-      <title>
-          Помощь зависимым и их близким, церковная алко- и наркореабилитация
-      </title>
-      <script src="https://api-maps.yandex.ru/2.0-stable/?load=package.standard&lang=ru-RU" type="text/javascript"></script>
-  </head>
+    <head>
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png?v=<?= time(); ?>">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png?v=<?= time(); ?>">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png?v=<?= time(); ?>">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png?v=<?= time(); ?>">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png?v=<?= time(); ?>">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png?v=<?= time(); ?>">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png?v=<?= time(); ?>">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png?v=<?= time(); ?>">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png?v=<?= time(); ?>">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png?v=<?= time(); ?>">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=<?= time(); ?>">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png?v=<?= time(); ?>">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=<?= time(); ?>">
+        <link rel="manifest" href="/manifest.json?v=<?= time(); ?>">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png?v=<?= time(); ?>">
+        <meta name="theme-color" content="#ffffff">
+        <meta charset="utf-8">
+        <meta name="format-detection" content="telephone=no">
+        <meta name="title" content="Помощь зависимым">
+        <meta name="keywords" content="костромская_митрополия, Скоропослушница, Литургия , молебен, катехизация, терапия, вера, наркозависимость, ресоциализация, адаптация, православная_реабилитация, скайп_группа, православие, кострома, итоги_недели, костромская_область, выздоровление, сумароково, взаимопомощь, реабилитация_наркозависимых, наркомания, алкоголизм, лечение_наркозависимых, группа_самопомощи, церковная помощь, группы_поддержки, мотивация, консультация, зависимость, помощь_зависимым, помощь_родственникам, наркореабилитация, алкореабилитация, здоровый_образ_жизни, адаптационная_квартира, реабилитационный_центр, реабилитация в другом регионе, трезвость, нет_наркотикам, без_наркотиков, верни_себе_жизнь, реабилитация_в_костроме, наркореабилитация_в_костроме">
+        <meta name="description" content="Помощь зависимым и их близким, церковная алко- и наркореабилитация">
+        <meta name="yandex-verification" content="5f434f86dd3e3a48" />
+        <meta name='wmail-verification' content='d7733a16c22ef0733ec321c297d3eb0d' />
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+        <title>
+            Помощь зависимым и их близким, церковная алко- и наркореабилитация
+        </title>
+        <script src="https://api-maps.yandex.ru/2.0-stable/?load=package.standard&lang=ru-RU" type="text/javascript"></script>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="/js/main.js?v=<?= time(); ?>" charset="utf-8"></script>
+    </body>
 </html>
